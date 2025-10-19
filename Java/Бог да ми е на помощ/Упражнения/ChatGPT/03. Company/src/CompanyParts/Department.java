4ac3e0241ffbedb47f6de7d95e8c098422abefd5
emo-nikoloff <emobg321@gmail.com>
2025-10-20 01:01:49 +0300

    Made some changes on the last task

--- Java/Бог да ми е на помощ/Упражнения/ChatGPT/03. Company/src/CompanyParts/Department.java
+++ Java/Бог да ми е на помощ/Упражнения/ChatGPT/03. Company/src/CompanyParts/Department.java
@@ -5,24 +5,30 @@ public class Department {
     private Employee[] employees;
     private int employeeCount;
 
-    public Department(String name, int employeeCount) {
+    public Department(String name, int capacity) {
         this.name = name;
-        this.employees = new Employee[employeeCount];
-        this.employeeCount = employeeCount;
+        this.employees = new Employee[capacity];
+        this.employeeCount = 0;
     }
 
     public void addEmployee(Employee emp) {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null) {
-                employees[i] = emp;
-                break;
-            }
+        if (employeeCount < employees.length) {
+            employees[employeeCount] = emp;
+            employeeCount++;
+            System.out.printf("Добавен е служител: %s в отдел %s%n", emp.getName(), name);
+        } else {
+            System.out.println("Отделът е пълен. Не може да се добавят повече служители.");
         }
     }
 
-    public void showAllEmployees() {
+    public void showDepartmentInfo() {
         System.out.printf("%s:%n", name);
-        for (int i = 0; i < employees.length; i++) {
+        if (employeeCount == 0) {
+            System.out.println("Няма служители в отдела!");
+            return;
+        }
+
+        for (int i = 0; i < employeeCount; i++) {
             employees[i].displayInfo();
         }
 
@@ -32,16 +38,24 @@ public class Department {
     }
 
     public double calculateAverageSalary() {
+        if (employeeCount == 0) {
+            return 0.0;
+        }
+
         double sum = 0;
-        for (int i = 0; i < employees.length; i++) {
+        for (int i = 0; i < employeeCount; i++) {
             sum += employees[i].getSalary();
         }
         return sum / employees.length;
     }
 
     public Employee findHighestSalaryEmployee() {
+        if (employeeCount == 0) {
+            return null;
+        }
+
         Employee bestEmp = employees[0];
-        for (int i = 1; i < employees.length; i++) {
+        for (int i = 1; i < employeeCount; i++) {
             Employee emp = employees[i];
             if (emp.getSalary() > bestEmp.getSalary()) {
                 bestEmp = emp;
--- Java/Бог да ми е на помощ/Упражнения/ChatGPT/03. Company/src/WorkField.java
+++ Java/Бог да ми е на помощ/Упражнения/ChatGPT/03. Company/src/WorkField.java
@@ -3,38 +3,25 @@ import CompanyParts.Employee;
 
 public class WorkField {
     public static void main(String[] args) throws Exception {
-        int employeeCount = 0;
+        Department department1 = new Department("Дръж Ми Шапката ЕООД", 5);
+        Department department2 = new Department("Клуб Уинкс ЕООД", 5);
 
         Employee emp1 = new Employee("Емилиян Николов", 12, "Staff Engineer", 420069.00);
-        employeeCount++;
+        department1.addEmployee(emp1);
 
         Employee emp2 = new Employee("Неджимидин Хюсеин", 34, "Junior Developer", 23000.45);
-        employeeCount++;
-
-        Department department1 = new Department("Дръж Ми Шапката ЕООД", employeeCount);
-        employeeCount = 0;
-
-        department1.addEmployee(emp1);
         department1.addEmployee(emp2);
 
-        department1.showAllEmployees();
-
         Employee emp3 = new Employee("Рожда Бекир", 123, "Staff Engineer", 69420.00);
-        employeeCount++;
+        department2.addEmployee(emp3);
 
         Employee emp4 = new Employee("Ива Тодорова", 456, "Senior Developer", 34000.00);
-        employeeCount++;
+        department2.addEmployee(emp4);
 
         Employee emp5 = new Employee("Сибел Камберова", 789, "Junior Developer", 17000.00);
-        employeeCount++;
-
-        Department department2 = new Department("Клуб Уинкс ЕООД", employeeCount);
-        employeeCount = 0;
-
-        department2.addEmployee(emp3);
-        department2.addEmployee(emp4);
         department2.addEmployee(emp5);
 
-        department2.showAllEmployees();
+        department1.showDepartmentInfo();
+        department2.showDepartmentInfo();
     }
 }
